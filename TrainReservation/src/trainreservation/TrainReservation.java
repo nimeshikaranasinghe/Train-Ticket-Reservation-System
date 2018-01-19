2c66ab790dce16ee71d35bcf855788d7aeb91446
Nimeshika Ranasinghe <nimeshika.ishani@gmail.com>
2018-01-20 01:46:34 +0530

    Java Application

---
+++ TrainReservation/src/trainreservation/TrainReservation.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trainreservation;
+
+/**
+ *
+ * @author H P
+ */
+public class TrainReservation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new ReservationMenu().setVisible(true);
+    }
+    
+}
